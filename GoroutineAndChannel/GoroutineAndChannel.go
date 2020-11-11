3235ccec80b62429f1bccbb538854e23099965ee
jay <hooney731@gmail.com>
2020-11-11 16:57:49 +0900

    channel buffer 추가가

--- GoroutineAndChannel/GoroutineAndChannel.go
+++ GoroutineAndChannel/GoroutineAndChannel.go
@@ -4,18 +4,21 @@ import "fmt"
 
 func GoFunc(writer chan string) {
 	// 채널에 값 전달
-	writer <- "jay"
+	writer <- `jay`
+	writer <- `hoon`
 }
 
 func main() {
 
 	// 채널 생성
-	writer := make(chan string)
+	writer := make(chan string, 2)
+
 
 	go GoFunc(writer)
 
 	// 채널 값 받아오기
-	myName := <- writer
-	fmt.Println(`my name is`, myName)
+	// myName := <- writer
+	fmt.Println(`my name is`, <- writer)
+	fmt.Println(`my name is`, <- writer)
 
 }
