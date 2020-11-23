9950ba2c346cab96dff9faac14f467d0e44a34f0
jay <hooney731@gmail.com>
2020-11-23 21:10:10 +0900

    CommandlineArgs 내용 추가 및 주석 추가

--- CommandlineFlag/CommandlineFlag.go
+++ CommandlineFlag/CommandlineFlag.go
@@ -10,10 +10,12 @@ var logger = fmt.Println
 func main() {
 	logger(`===== COMMANDLINE FLAG =====`)
 
+	// 옵션값 - 기본값 - 설명 순서.
 	wordPtr := flag.String("word", "foo", "a string")
 	numPtr := flag.Int("num", 731, "an int")
 	boolPtr := flag.Bool("fork", false, "a bool")
 
+	// 다른 곳에서 선언된 변수를 사용해 옵션을 선언하는것도 가능.
 	var svar string
 	flag.StringVar(&svar, "svar", "bar", "a string var")
 
