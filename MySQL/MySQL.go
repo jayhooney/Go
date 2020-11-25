8602445e21963a077f3cf3c5924adae229ddfc6b
jay <hooney731@gmail.com>
2020-11-25 22:46:21 +0900

    쿼리 예제 실습 내용 추가

--- MySQL/MySQL.go
+++ MySQL/MySQL.go
@@ -1,11 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 
-	"./secret"
+	_ "github.com/go-sql-driver/mysql"
+
+	secret "./secret"
 )
 
+type Topic struct {
+	DOCID int
+	TERMS string
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
 func main() {
-	fmt.Println(secret.GetDBInfo())
+	db, err := sql.Open(secret.GetEngine(), secret.GetDBInfo())
+	CheckErr(err)
+	defer db.Close()
+
+	rows, err := db.Query(secret.GetQuery())
+	CheckErr(err)
+	defer rows.Close()
+
+	var topic Topic
+	for rows.Next() {
+		err := rows.Scan(&topic.DOCID, &topic.TERMS)
+		CheckErr(err)
+
+		log.Println(topic.DOCID, topic.TERMS)
+	}
+
 }
